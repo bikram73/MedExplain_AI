4670adb2deabda42cd5a940cd97c62d857c76403
bikram73 <bikram8548@gmail.com>
2026-05-21 22:52:42 +0530

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,8 @@ import { defineConfig } from "vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
+import tailwind from "@tailwindcss/vite";
 
 export default defineConfig({
-  plugins: [tanstackStart(), react(), tsconfigPaths()],
+  plugins: [tailwind(), tanstackStart(), react(), tsconfigPaths()],
 });
